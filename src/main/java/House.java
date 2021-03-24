52353ee00316b411829c1dac08cdca4f62597b14
Richard Mathis <achat@anothem.com>
2021-03-24 11:43:03 +0100

    1st

---
+++ src/main/java/House.java
@@ -0,0 +1,17 @@
+public class House {
+
+
+    public static void main(String args[]) {
+
+        int number = 4;
+        int nb;
+
+        nb = 1;
+        nb +=number;
+
+        if(nb > 4 ) {
+            System.out.println(("nb > 4"));
+        }
+    }
+
+}
