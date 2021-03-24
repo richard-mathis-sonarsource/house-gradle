d3db8445f50e4bb9065bf607a06ff3e50d0c37b6
Richard Mathis <achat@anothem.com>
2021-03-24 11:44:38 +0100

    1st

---
+++ src/main/java/Address.java
@@ -0,0 +1,21 @@
+
+//import lombok.AccessLevel;
+//import lombok.Getter;
+//import lombok.Setter;
+
+public class Address {
+
+    //@Getter @Setter
+    private String street;
+    private int number;
+    private int zipcode;
+    private String city;
+
+    public Address(String street, int number, int zipcode, String city) {
+        this.street = street;
+        this.number = number;
+        this.zipcode = zipcode;
+        this.city = city;
+    }
+
+}
